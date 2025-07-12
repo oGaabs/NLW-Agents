e9dcaf765fdd4cd894ff182e21707191012de142
Gabriel Santana <gabriel04.ok@gmail.com>
2025-07-12 18:58:36 -0300

    add: room listing route

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import { fastify } from 'fastify';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+import { sql } from './db/connection.ts';
+import { fastifyCors } from '@fastify/cors';
+import { env } from './env.ts';
+import { getRoomsRoute } from './routes/get-rooms.ts';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: 'http://localhost:5137',
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.get('/health', async () => {
+  return 'OK';
+});
+
+app.register(getRoomsRoute);
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log(`HTTP server running on http://localhost:${env.PORT}`);
+});
