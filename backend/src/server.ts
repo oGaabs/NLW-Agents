dbb0a200c5b946137872a0a9a51d34c404ad311f
Gabriel Santana <gabriel04.ok@gmail.com>
2025-07-12 22:56:48 -0300

    add: frontend app setup, rooms listing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,7 @@ import { getRoomsRoute } from './routes/get-rooms.ts';
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-  origin: 'http://localhost:5137',
+  origin: 'http://localhost:5173',
 });
 
 app.setSerializerCompiler(serializerCompiler);
